b5c85204212d58228e32542564a461e739ddebe7
Edouard Cuny <edouardcuny@MacBook-Pro-de-Edouard.local>
2017-04-12 14:49:15 +0200

    ajout du lemmatiseur

---
+++ lemmatiseur/baca.py
@@ -0,0 +1,4 @@
+from lemmatizeur import Lemmatiseur
+
+lemmatiseur = Lemmatiseur()
+print(lemmatiseur.lemmatize("marchais"))
---
+++ lemmatiseur/lemmatizeur.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+class Lemmatiseur():
+
+    def __init__(self):
+        self.hostname = 'localhost'
+        self.username = 'edouardcuny'
+        self.database = 'lemmatiseur'
+        self.conn = psycopg2.connect( host = self.hostname, user = self.username, dbname = self.database )
+        self.cur = self.conn.cursor()
+
+    def lemmatize(self, word):
+        self.cur.execute( "SELECT lemme FROM liste_lemme WHERE word = '{0}' ".format(word) )
+        return(self.cur.fetchall()[0][0])
