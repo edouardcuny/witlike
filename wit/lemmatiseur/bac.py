0f672dcd4a01551a20b3b4eb9b2cff01411f3fb6
edouardcuny <edouardcuny@gmail.com>
2017-04-20 16:39:56 +0200

    second way to compute class - using random forest

--- wit/lemmatiseur/bac.py
+++ wit/lemmatiseur/bac.py
@@ -1,6 +1,14 @@
-from lemmatizeur import Lemmatiseur
+# from lemmatizeur import Lemmatiseur
+#
+# lemmatiseur = Lemmatiseur()
+# print(lemmatiseur.lemmatize("mets"))
+# print(lemmatiseur.lemmatize("éteins"))
+# print(lemmatiseur.lemmatize("azzz"))
 
-lemmatiseur = Lemmatiseur()
-print(lemmatiseur.lemmatize("mets"))
-print(lemmatiseur.lemmatize("éteins"))
-print(lemmatiseur.lemmatize("azzz"))
+a = [1, 2, 3]
+b = a[a==1]
+print(b)
+
+vec = [2,2,3,4]
+idx = vec.index(4)
+print(idx)
--- wit/wit.py
+++ wit/wit.py
@@ -5,6 +5,13 @@ import os
 import numpy as np
 from intent import Intent
 import pandas as pd
+import toolkit as tk # module de nlp perso
+from sklearn.ensemble import RandomForestClassifier # classifier
+from sklearn.multiclass import OneVsRestClassifier
+import random
+
+random.seed(7)
+
 
 # importation du W2V
 os.chdir("/Users/edouardcuny/Desktop/witlike/wit")
@@ -53,6 +60,9 @@ class Wit():
             self.key_words += intent.key_words
         self.key_words = list(set(self.key_words)) # supprime duplicates
 
+        self.model = RandomForestClassifier()
+
+
     def train_wit(self):
         # à modifier de façon à ce que le seuil se fixe de façon automatique
         self.threshold = 0.7
@@ -65,7 +75,7 @@ class Wit():
         intent = Intent(word_to_vec = self.word_to_vec, nom = nom)
         self.intents.append(intent)
 
-    def classify_intent(self, sentence):
+    def classify_intent_1(self, sentence):
         '''
         Renvoie pour 'sentence' l'intent avec le score maximal.
         NB = pour l'instant pas d'histoire de threshold cad qu'il renverra
@@ -102,22 +112,103 @@ class Wit():
                 return intent
         print(strintent.upper()," pas d'intent trouvé")
 
-    # def s2v(self, sentence):
+    def s2v(self, sentence):
+        '''
+        Sentence2Vec 'maison'
+        Prend une phrase en entrée
+        Renvoie un vecteur de la même longueur que les key_words de wit
+        avec des 1 si le key_word est dans la phrase 0 sinon
+        '''
+        sentence = tk.prepare_sentence(sentence)
+        vec = np.zeros(len(self.key_words))
+        for i in range(len(self.key_words)):
+            if self.key_words[i] in sentence:
+                vec[i] = 1
+        return vec
+
+    def i2v(self, intent):
+        '''
+        Intent2Vec
+        Prend un intent, renvoie un entier correspondant à l'index de
+        cet intent dans self.intents
+        '''
+        for i in range(len(self.intents)):
+            if self.intents[i].nom == intent:
+                return i
+
+    def v2i(self, i):
+        '''
+        Vector2Intent
+        Prend un entier
+        et le transforme en intent
+        '''
+        return(self.intents[i].nom)
+
+    def fit(self, x_train, y_train):
+        '''
+        x_train = df.series de phrases
+        y_train = df.series d'intent
+        '''
+
+        x_train = x_train.apply(lambda x : self.s2v(x))
+        x_train = x_train.values.flatten().tolist()
+        x_train = np.array(x_train)
+
+        # print("x_train", x_train)
+
+        y_train = y_train.apply(lambda x : self.i2v(x))
+        y_train = y_train.values.flatten().tolist()
+        y_train = np.array(y_train)
+
+        # print("y_train", y_train)
+
+        self.model.fit(x_train, y_train)
+        return None
 
 
+    def classify_intent_2(self, x_test):
+        '''
+        x_test = df.series phrase
+        '''
 
+        x_test = x_test.apply(lambda x : self.s2v(x))
+        x_test = x_test.values.flatten().tolist()
+        x_test = np.array(x_test)
 
+        vec = self.model.predict(x_test)
 
-# je fais le test sur le csv écrit à la main
+        # on convertit chaque prev du format 1-hot vector en intent
+        vec = vec.tolist()
+        for i in range(len(vec)):
+            vec[i] = self.v2i(vec[i])
+
+        return(vec)
+
+# je crée mon objet wit (à mettre dans un main après)
 l = [lux, ratp, humour, music]
 witlike = Wit (model, *l)
-test = pd.read_excel("train_intent.xlsx", sep = ";")
 
-print(witlike.key_words)
+# entraînement
+test = pd.read_excel("train_intent_only_intents.xlsx", sep = ";")
+x_train = test.ix[:,0]
+y_train = test.ix[:,1]
+witlike.fit(x_train, y_train)
+
+# classification
+pred = witlike.classify_intent_2(x_train)
+x_train = pd.DataFrame(x_train)
+x_train["pred"] = pred
+x_train["label"] = y_train
+x_train.to_csv("rf.csv")
+
+
+
+
+
 
 # RENVOIE LA CLASSE RETENUE POUR CHAQUE SENTENCE
-test["pred"] = test["phrase"].apply(witlike.classify_intent)
-print(test)
+# test["pred"] = test["phrase"].apply(witlike.classify_intent)
+# print(test)
 
 # RENVOIE LE SCORE POUR CHAQUE INTENT
 # test["pred"] = test["phrase"].apply(witlike.classify_intent)
