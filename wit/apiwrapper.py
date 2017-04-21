e4bced70c006782ddb23b1850da805a2cb754997
edouardcuny <edouardcuny@gmail.com>
2017-04-21 15:16:01 +0200

    feature = entrainement wit by post in webapp

--- wit/apiwrapper.py
+++ wit/apiwrapper.py
@@ -1,8 +1,10 @@
 # coding : utf-8
 
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, jsonify
+
 import numpy as np
 import pandas as pd
+import pickle
 
 from wit import Wit
 
@@ -16,3 +18,23 @@ class Api():
         def classify_intent():
             x_test = pd.Series(request.json['sentence'])
             return(self.wit.classify_intent_2(x_test)[0])
+
+        @self.app.route('/get_intents')
+        def get_intents():
+            l = []
+            for intent in self.wit.intents:
+                l.append(intent.nom)
+            return jsonify(intents = l)
+
+        @self.app.route('/train', methods = ['POST'])
+        def train():
+            try:
+                x_train = pd.Series(request.json['x_train'])
+                y_train = pd.Series(request.json['y_train'])
+
+            except:
+                return("Input of Post not good format")
+
+            self.wit.fit(x_train, y_train)
+            pickle.dump( self.wit, open( "wit.p", "wb" ))
+            return("The model was just trained")
--- wit/bas.py
+++ wit/bas.py
@@ -1,10 +1,14 @@
 # coding : utf-8
 
+import json
 import pandas as pd
 import os
 
 os.chdir("/Users/edouardcuny/Desktop/witlike/wit")
-print(os.getcwd())
-test = pd.read_excel("train_intent.xlsx", sep = ";")
-test["pred"] = test["phrase"].apply(lambda x : x + "edouard")
-print(test)
+train = pd.read_excel("train_intent_only_intents.xlsx", sep = ";")
+x_train = train.ix[:, 0]
+x_train = x_train.tolist()
+y_train = train.ix[:, 1]
+y_train = y_train.tolist()
+
+print(json.dumps( {{"x_train" : x_train},{"y_train" : y_train}}))
